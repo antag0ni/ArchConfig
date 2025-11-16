bc0ea60ab4b3189f5ac496ba3b757b8ddb7c9dfe
antag0ni <gionatan.conti04@gmail.com>
2025-11-16 22:01:02 +0100

    removed local

--- install.sh
+++ install.sh
@@ -11,8 +11,8 @@ echo "[*] Arch Post-Install Script"
 echo "[*] Modules enabled: ${MODULES[*]}"
 
 for module in "${MODULES[@]}"; do
-    local module_name="$module"
-    local module_path="modules/${module_name}.sh"
+    module_name="$module"
+    module_path="modules/${module_name}.sh"
 
     if [[ -f "$module_path" ]]; then
         echo "[*] Running module: $module_name"
